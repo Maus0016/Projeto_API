fa436b744022b7012b3f6bd668f54537997ffa54
Daniel Maus <hakumennomodo@gmail.com>
2025-10-20 20:58:25 -0300

    Teste

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
+const baseUrl = "https://localhost:7134"
 async function get(){
-    const res = await fetch("https://localhost:7134/api/Usuario")
+    const res = await fetch(`${baseUrl}/api/Usuario`)
 }
 get()
+async function create(params) {
+    const res = await fetch("https://localhost:7134/api/Usuario")
+}
