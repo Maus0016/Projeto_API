a8da9a9db98205f9f1d25a549c53f8ca9fd52470
Daniel Maus <hakumennomodo@gmail.com>
2025-10-28 20:55:47 -0300

    Atusalizando o Script...

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const baseUrl = "https://localhost:7134"
-async function get(){
+async function get() {
     const res = await fetch(`${baseUrl}/api/Usuario`)
     const users = await Response.json
 }
@@ -8,9 +8,9 @@ async function create(params) {
     const res = await fetch("https://localhost:7134/api/Usuario")
 }
 
-function init(){
+function init() {
     const form = document.querySelector("form")
-    form.addEventListener("submit", (event)=>{
+    form.addEventListener("submit", (event) => {
         event.preventDefault()
         createuser()
 
@@ -18,30 +18,59 @@ function init(){
 }
 init()
 
-function abrirModal(){
+function abrirModal() {
     const body = document.body
 
     body.insertAdjacentElement("beforeend")
 }
 
-function toastify(tipo,mensagem){
-    document.body.insertAdjacentHTML("beforeend",`
+function toastify(tipo, mensagem) {
+    document.body.insertAdjacentHTML("beforeend", `
         <div class="toastify ${tipo}">
         <p>${mensagem}<p>
         </div>
-    `)
-    const toas= document.querySelector(".toastify")
+        `)
+    const toas = document.querySelector(".toastify")
     setTimeout(() => {
 
         toas.remove()
     }, 3000);
 }
- async function createuser(){
+async function createUsuario() {
     const name = document.querySelector("#name")
     const senha = document.querySelector("#senha")
     const usuario = {
-        name:name.value,
-        senha:senha.value
-    } 
- }
- 
+        name: name.value,
+        senha: senha.value
+    }
+    const response = await fetch(`${baseUrl}/users`,
+        {
+            method: "POST",
+            headers: headers,
+            body: JSON.stringify(usuario)
+        })
+
+    console.log(response, "response")
+    if (response.ok) {
+        const UsuarioS = await response.json()
+        console.log(UsuarioS, "users")
+        toastify("Sucesso", "Usuario ou senha invalidos.")
+    }
+    else {
+        toastify("Sucesso", "Login efetuado com sucesso!")
+    }
+
+}
+
+async function removeUsuario() {
+
+    const response = await fetch(`${baseUrl}/users/1`,
+        {
+            method: "DELETE"
+        })
+    console.log(response, "response delete")
+}
+
+
+
+
