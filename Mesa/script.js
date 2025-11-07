168b8992cf688f86e85e56d98907c3d5cbc7fb29
Daniel Maus <hakumennomodo@gmail.com>
2025-11-07 19:36:10 -0300

    Mesa vinculada a API

--- Mesa/script.js
+++ Mesa/script.js
@@ -1,5 +1,31 @@
-const baseUrl = "https://localhost:7134"
-async function get() {
-    const res = await fetch(`${baseUrl}/api/Mesa`)
-    const mesa = await response.json
+const baseUrl = "http://localhost:5042"
+const headers = {
+    "Content-Type": "application/json"
 }
+async function get() {
+    const res = await fetch(`${baseUrl}/api/Mesa`, {
+        headers: headers
+    })
+    console.log(res, "res")
+    const mesa = await res.json()
+    console.log(mesa, "mesas")
+    const container = document.querySelector(".container")
+    mesa.forEach(mesa => {
+        let situacao = "Disponivel"
+        if (mesa.situacaoMesa === 1) {
+            situacao = "Ocupada"
+        } else if (mesa.situacaoMesa === 2) {
+
+        }
+        container.insertAdjacentHTML("beforeend", `
+              <div class="mesa">
+            <p>Mesa ${mesa.numeroMesa}</p>
+            <p>Status: ${situacao}</p>
+            <button>Ocupar Mesa</button>
+        </div>
+            
+            `)
+
+    })
+}
+get()
