995bdc13293f6ae1de013ac2bbaf8cb3cd3e7170
Daniel Maus <hakumennomodo@gmail.com>
2025-11-21 20:23:15 -0300

    Comanda Itens

--- Comanda/script.js
+++ Comanda/script.js
@@ -109,37 +109,76 @@ function openCreateModal() {
             <div class="wrapper">
                 <div class="modal">
                     <h3>Criar Comanda</h3>
+                    <ul id="itens">
+                    </ul>
                     <input type="number" placeholder="Número da mesa" id="numeroMesa">
                     <input type="text" placeholder="Nome do cliente" id="nomeCliente">
+                    <input type="text" placeholder="Itens" id="comandaItens" id="itens[]"/>
+                    <label for="itens">Itens (separados por vírgula)</label>
+                    
 
                     <button id="createBtn">Salvar</button>
                     <button id="closeModal">Cancelar</button>
                 </div>
             </div>
         `);
+        getCardapioItens();
 
-        document.getElementById("closeModal").onclick = closeModals;
+        async function getCardapioItens() {
+            const res = await fetch(`${baseUrl}/api/CardapioItem`, { headers });
+            const cardapioitens = await res.json();
+            const itensContainer = document.getElementById("itens");
 
-        document.getElementById("createBtn").onclick = async () => {
-            const Comanda = {
-                numeroMesa: Number(document.getElementById("numeroMesa").value),
-                nomeCliente: document.getElementById("nomeCliente").value
-            };
-
-            const response = await fetch(`${baseUrl}/api/Comanda`, {
-                method: "POST",
-                headers,
-                body: JSON.stringify(Comanda)
+            cardapioitens.forEach(item => {
+                itensContainer.insertAdjacentHTML("beforeend", `
+                    <li>
+                        <label for="item-${item.id}">${item.nome}</label>
+                        <input type="checkbox" id="item-${item.id}" value="${item.id}" class="item-checkbox"/>
+                    </li>
+                `);
             });
+            //cardaoioitebs chama
+            //=[]
+            //get cardapioitens from api/cardapioitens
+            //foreach item in array 
+            //itens
+            //<label>${item.nome}<label/>
+            //<inout class="check" id="caitem" type="chcekbox"/> 
 
-            if (response.ok) {
-                closeModals();
-                get();
-            }
-        };
-    });
+            document.getElementById("closeModal").onclick = closeModals;
+
+            document.getElementById("createBtn").onclick = async () => {
+                // const selectedItems = Array.from(document.querySelectorAll(".item-checkbox:checked")).map(cb => Number(cb.value));
+                const itensSelecionados = [];
+                const check = document.querySelectorAll(".item-checkbox");
+                check.forEach(item => {
+                    if (item.checked) {
+                        itensSelecionados.push(Number(item.value));
+                    }
+                })
+                console.log(itensSelecionados, "itens selecionados");
+                const Comanda = {
+                    numeroMesa: Number(document.getElementById("numeroMesa").value),
+                    nomeCliente: document.getElementById("nomeCliente").value,
+                    cardapioItemIds: itensSelecionados
+                };
+
+                const response = await fetch(`${baseUrl}/api/Comanda`, {
+                    method: "POST",
+                    headers,
+                    body: JSON.stringify(Comanda)
+                });
+
+                if (response.ok) {
+                    closeModals();
+                    get();
+                }
+            };
+        }
+
+
+    })
 }
-
 openCreateModal();
 
 // =========================
